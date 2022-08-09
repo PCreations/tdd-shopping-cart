70633723e6739954f4fb5c8f88e8da565af280a2
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2022-08-09 21:18:06 +0200

    step1: tell the story

---
+++ src/__tests__/add-product-to-cart.spec.ts
@@ -0,0 +1,38 @@
+describe("Feature: Adding a product to the cart", () => {
+  let sut: Sut;
+
+  beforeEach(() => {
+    sut = createSut();
+  });
+  describe("Rule: Should add a quantity of one product when adding a product", () => {
+    test('Example: Adding "mustard" at 2.5€ in the cart', async () => {
+      sut.givenExistingProduct({
+        id: "mustard",
+        price: 2.5,
+      });
+      sut.givenCart({
+        products: [],
+        total: 0,
+      });
+
+      await sut.whenAddingProductInCart({
+        id: "mustard",
+      });
+
+      sut.thenCartShouldBe({
+        products: [
+          {
+            id: "mustard",
+            quantity: 1,
+            price: 2.5,
+          },
+        ],
+        total: 2.5,
+      });
+    });
+  });
+});
+
+const createSut = () => {};
+
+type Sut = ReturnType<typeof createSut>;
